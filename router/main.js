5d2ec02cbbe4b52a949dda8c9e985529e4d61774
inbyul <89333548+inbyul@users.noreply.github.com>
2021-11-04 06:39:28 +0000

    어저라고ㅉㅈ

--- router/main.js
+++ router/main.js
@@ -6,4 +6,16 @@ module.exports = function(app)
      app.get('/talk',function(req,res) {
         res.render('talk.html')
      });
+      app.get('/register',function(req,res) {
+        res.render('register.html')
+      });
+      app.get('/talk',function(req,res) {
+        res.render('talk.html')
+      });
+      app.get('/close',function(req,res) {
+        res.render('close.html')
+      });
+      app.get('/admin',function(req,res) {
+        res.render('secret.html')
+      })
 }
